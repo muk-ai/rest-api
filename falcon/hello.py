a46e92fc2d4f3f966b702f2d5904e41c4a74f080
root <root@ip-172-31-19-37.ap-northeast-1.compute.internal>
2016-04-07 11:54:52 +0000

    Change message

--- falcon/hello.py
+++ falcon/hello.py
@@ -4,7 +4,7 @@ import json
 class Index:
     def on_get(self, req, resp):
         msg = {
-            "messages": "hello, world"
+            "messages": "goodbye, world"
         }
         resp.status = falcon.HTTP_200
         resp.body = json.dumps(msg)

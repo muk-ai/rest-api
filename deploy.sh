9855805b72bc60b257d6afac46ad571d0e8d977e
Kenta Mukai <kenta@muk.ai>
2016-04-15 06:16:05 +0000

    Add port check

--- deploy.sh
+++ deploy.sh
@@ -8,16 +8,34 @@ if [ -z $1 ]; then
 fi
 image=$1
 
-container_id_long=$(docker run -d -it $image)
+docker pull $image
 if [ $? -ne 0 ]; then
-  echo "docker run failed"
+  echo "docker pull $image failed"
   exit 1
 fi
-container_id=$(echo $container_id_long | cut -c -12)
 
-# 5秒以内に立ち上がらんかったら知らん
-sleep 5
+container_id_long=$(docker run -d $image)
+if [ $? -ne 0 ]; then
+  echo "docker run $image failed"
+  exit 1
+fi
+container_id_short=$(echo $container_id_long | cut -c -12)
+container_ip=$(docker exec $container_id_short ip a | grep 'inet ' | fgrep -v '127.0.0.1' | awk '{ print $2 }' | head -1 | awk -F '/' '{ print $1 }')
 
-docker network connect --alias $container_id $network_name $container_id
-curl http://localhost/container/switchover?dest=$container_id
+nc -zv $container_ip 3031
+if [ $? -ne 0 ]; then
+  sleep 3
+  nc -zv $container_ip 3031
+  if [ $? -ne 0 ]; then
+    echo "port 3031 check failed"
+    exit 1
+  fi
+fi
+
+docker network connect --alias $container_id_short $network_name $container_id_short
+curl http://localhost/container/switchover?dest=$container_id_short
 curl http://localhost/
+if [ $? -eq 0 ]; then
+  echo
+  echo "deploy succeeded"
+fi

70e3092c22717bfaeffda837a9e9e7022023ee3c
Kenta Mukai <kenta@muk.ai>
2016-05-25 13:15:20 +0000

    fix3

--- falcon/rest-api-example/db.py
+++ falcon/rest-api-example/db.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 db_url = 'mysql://{0}:{1}@{2}/{3}'.format(
-    os.environ['MYSQL_USER'],
-    os.environ['MYSQL_PASSWORD'],
-    os.environ['MYSQL_HOST'],
-    os.environ['MYSQL_DATABASE']
+    os.environ.get('MYSQL_USER', ''),
+    os.environ.get('MYSQL_PASSWORD', ''),
+    os.environ.get('MYSQL_HOST', 'localhost'),
+    os.environ.get('MYSQL_DATABASE', '')
 )
 engine = create_engine(db_url, echo=True)
 Session = sessionmaker(bind=engine)
